3b63bc39dbfc8b9871686a55d9509b26e845af7c
LambdaXymox <lambda.xymox@gmail.com>
2018-11-18 01:22:08 -0600

    Expand example to illustrate the difference between installation values and system values.

--- examples/example.rs
+++ examples/example.rs
@@ -2,7 +2,11 @@ extern crate xdg;
 
 
 fn main() {
-    let config = xdg::get_config_or_default();
+    let config_with_defaults = xdg::get_config_or_default();
+    println!("CONFIGURATION USED FOR INSTALLATION:");
+    println!("{}", config_with_defaults);
 
-    println!("{}", config); 
+    let config_without_defaults = xdg::get_config();
+    println!("DEFAULT VALUES OF XDG VARIABLES");
+    println!("{}", config_without_defaults);
 }
