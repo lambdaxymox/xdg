f482d9a2f2299a708499d84cf7022e8ba1a7315c
LambdaXymox <lambda.xymox@gmail.com>
2018-11-18 01:25:09 -0600

    Add documentation to example.

--- examples/example.rs
+++ examples/example.rs
@@ -2,11 +2,15 @@ extern crate xdg;
 
 
 fn main() {
+    // Query the XDG environment variables for software installation.
+    // If some values are missing, we will get the defaults specified in the
+    // XDG Base Directory standard.
     let config_with_defaults = xdg::get_config_or_default();
-    println!("CONFIGURATION USED FOR INSTALLATION:");
+    println!("XDG CONFIGURATION USED FOR INSTALLATION:");
     println!("{}", config_with_defaults);
 
+    // Query the XDG environment variables as they are. 
     let config_without_defaults = xdg::get_config();
-    println!("DEFAULT VALUES OF XDG VARIABLES");
+    println!("CURRENT VALUES OF XDG VARIABLES");
     println!("{}", config_without_defaults);
 }
