54e731851ceef1d18c0c7e45bf772ba3ada12c52
LambdaXymox <lambda.xymox@gmail.com>
2018-11-17 22:47:02 -0600

    Make test stateless for env.

--- tests/test_xdg.rs
+++ tests/test_xdg.rs
@@ -3,13 +3,17 @@ extern crate xdg;
 use std::env;
 
 
-fn clear_xdg_config() {
+fn clear_xdg_config() -> xdg::Config {
+    let old_config = xdg::get_config();
+    
     env::set_var("XDG_DATA_HOME", "");
     env::set_var("XDG_CONFIG_HOME", "");
     env::set_var("XDG_DATA_DIRS", "");
     env::set_var("XDG_CONFIG_DIRS", "");
     env::set_var("XDG_CACHE_HOME", "");
     env::set_var("XDG_RUNTIME_DIR", "");
+
+    old_config
 }
 
 fn empty_xdg_config() {
@@ -24,6 +28,25 @@ fn empty_xdg_config() {
 fn no_xdg_runtime_dir_config() {
     env::remove_var("XDG_RUNTIME_DIR");
 }
+
+fn set_var_opt(key: &str, val: &Option<String>) {
+    let unwrapped = match val {
+        &Some(ref st) => st.clone(),
+        &None => String::from(""),
+    };
+
+    env::set_var(key, unwrapped);
+}
+
+fn reset_xdg_config(old_config: &xdg::Config) {
+    set_var_opt("XDG_DATA_HOME", &old_config.xdg_data_home);
+    set_var_opt("XDG_CONFIG_HOME", &old_config.xdg_config_home);
+    set_var_opt("XDG_DATA_DIRS", &old_config.xdg_data_dirs);
+    set_var_opt("XDG_CONFIG_DIRS", &old_config.xdg_config_dirs);
+    set_var_opt("XDG_CACHE_HOME", &old_config.xdg_cache_home);
+    set_var_opt("XDG_RUNTIME_DIR", &old_config.xdg_runtime_dir);
+}
+
 
 fn default_config() -> xdg::Config {
     let home = env::var("HOME").unwrap();
@@ -40,9 +63,10 @@ fn default_config() -> xdg::Config {
 
 #[test]
 fn test_get_config_or_default_should_generate_default_values_if_blank() {
-    clear_xdg_config();
+    let old_config = clear_xdg_config();
     let result = xdg::get_config_or_default();
     let expected = default_config();
+    reset_xdg_config(&old_config);
     
     assert_eq!(result, expected);
 }
